d838eb73daa0a5580f34cbf1fed341e807fd1832
Harukaah <140624361+Harukaah@users.noreply.github.com>
2023-12-19 15:30:18 +0700

    Update lab10_3.cpp

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,46 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<fstream>
+#include<iomanip>
+#include<cmath>
+using namespace std;
+
+int main (){
+    ifstream file;
+    file.open("score.txt");
+
+    double sum = 0;
+    double sumSquaredDiff = 0;
+    double value;
+    int count = 0;
+
+    while (file >> value) {
+        sum += value;
+        sumSquaredDiff += value * value;
+        count++;
+    }
+
+    file.close();
+
+    if (count == 0) {
+        cout << "No data in the file." << endl;
+        return 1; 
+    }
+
+    double mean = sum / count;
+    double variance = (sumSquaredDiff / count) - (mean * mean);
+    double stdDeviation = sqrt(variance);
+
+    cout << fixed << setprecision(3); 
+
+    cout << "Number of data = " << count << endl;
+
+    if (static_cast<int>(mean * 1000) % 10 == 0) {
+        cout << "Mean = " << showpoint << setprecision(2) << mean << endl;
+    } else {
+        cout << "Mean = " << showpoint << mean << endl;
+    }
+
+    cout << "Standard deviation = " << fixed << setprecision(3) << stdDeviation << endl;
+
+    return 0;
+}
